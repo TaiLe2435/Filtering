c672524c24ff6cb8f7713d9ad667d0767e0357e6
Tommy Le <TaiLe2435@gmail.com>
2023-08-18 12:10:52 -0500

    adding report

--- EKF.m
+++ EKF.m
@@ -7,11 +7,12 @@ num_iterations = 100;
 
 % ode with initial conditions 
 i1 = [0 0 0];
-v = 10; 
+v = 10;                     % change v and w to make square path
 w = 2;
 
 % ground truth data
-[t,gt] = ode45(@(t,gt) unicycle_ode(t, gt, v, w),tspan,i1);
+[t,gt] = ode45(@(t,gt) unicycle_ode(t, gt, v, w),tspan,i1); % need to update model to simulate pauses
+t = [t; t(end)+dt];
 
 w0 = [0.05; 0.05; pi/8]; % m, m, rad
 
@@ -20,7 +21,7 @@ gt = gt + [randn(size(gt,1), 1)*w0(1) ...
            randn(size(gt,1), 1)*w0(3)];
 
 Q_accel = 0.1;   % m/s^2 0.03
-Q_gyro = pi/16; % rad/s   pi/16
+Q_gyro = pi/(16^2); % rad/s   pi/16
 
 % simulated IMU measurement
 z = imu(w, v, gt, dt) + [randn(size(gt,1)+1, 1)*Q_accel ... 
@@ -34,6 +35,51 @@ for i=1:num_iterations
     drift = drift + pi/360; % drift around 180deg/hr | 0.5deg/sec
 end
 
+% ================== static data ===================================
+setup = [randn(size(gt,1)+1, 1)*Q_accel ... 
+        randn(size(gt,1)+1, 1)*Q_accel ...
+        randn(size(gt,1)+1, 1)*Q_gyro];
+
+drift = 0;
+for i=1:size(gt)
+    setup(i, 3) = setup(i, 3) + drift;
+    drift = drift + pi/360; % drift around 180deg/hr | 0.5deg/sec
+end
+
+% numerically differentiate to get simulated imu data
+vx = diff(setup(:,1)) ./ diff(t(size(setup(:,1))));
+ax = diff(vx) ./ diff(t(size(vx)));
+
+vy = diff(setup(:,2)) ./ diff(t(size(setup(:,2))));
+ay = diff(vy) ./ diff(t(size(vy)));
+
+wz = diff(setup(:,3)) ./ diff(t(size(wz)));
+
+% signal preprocessing
+bias = zeros(size(setup, 1), 3);
+bias = biasCorrection(setup(:,1), setup(:,2), wz); % param 1 and 2 need to be updated to wx and wy
+wzBias = bias(1,3);
+wzStatic = wz - wzBias;
+
+% NMNI
+wTh = NMNI(setup(:,1), setup(:,2), wzStatic);      % param 1 and 2 need to be updated to wx and wy
+
+% NDZTA
+aTh = NDZTA(ax, ay, setup(:,3));                   % param 3 needs to be updated to az
+
+% logic for zero update
+for i=1:length(wzStatic)
+    if abs(z(i,1)) < aTh(1,1)
+       z(i,1) = 0;
+    end
+    if abs(z(i,2)) < aTh(1,2)
+        z(i,2) = 0;
+    end
+    if abs(z(i,3)) < wTh(1,3)
+        z(i,3) = 0;
+    end
+end
+
 % figure(10);
 % hold on;
 % plot(z);
@@ -44,7 +90,7 @@ x0(1:2,1)=gt(1,1:2);
 x0(3,1)=0;
 P0=eye(3);
 
-ekf(dt, z, x0, P0, v, w, gt)
+ekf(dt, z, x0, P0, v, w, gt) % this z still has bias in gyro
 
 % ===================== Functions ============================
 
@@ -230,5 +276,62 @@ function show_plots(gt, z, xk, zModel)
     xlim([-inf inf])
     ylim([-inf inf])
     hold off;
+
+end
+
+% ===================== Preprocessing functions ===========================
+
+function Bias = biasCorrection(wx, wy, wz) % Bias correction for gyro drift compensation
+
+    wSum = zeros(1, 3);
+    wSum = [sum(wx) sum(wy) sum(wz)];
+    n = length(wz);
+
+    wxBias = wSum(1,1)/n;
+    wyBias = wSum(1,2)/n;
+    wzBias = wSum(1,3)/n;
+
+    Bias = [wxBias, wyBias, wzBias];
+
+end
+
+function wTh = NMNI(wx, wy, wz) % Signal processing for gyro
+    % function should be called when object is stationary and then be
+    % passed sampled data and return new thresholds
+
+    wxTh = max(wx);
+    wyTh = max(wy);
+    wzTh = max(wz);
+
+    wTh = [wxTh, wyTh, wzTh];
+
+end
+
+function aTh = NDZTA(ax, ay, az) % Signal processing for acc
+    % function should be called when object is stationary, then it should
+    % be passed sampled data and return new thresholds
+   
+    axTh = max(ax);
+    ayTh = max(ay);
+    azTh = max(az);
+
+    aTh = [axTh, ayTh, azTh];
+
+end
+
+function[px, py, pz] = areaIntegration(ax, ay, az)
+    dt = 0.2;
+
+    for i=1:length(ax) 
+        vx = vx_ + 0.5*(ax(i) + ax(i+1))*dt;
+        vy = vy_ + 0.5*(ay(i) + ay(i+1))*dt;
+        vz = vz_ + 0.5*(az(i) + az(i+1))*dt;
+    end
+
+    for j=1:length(vx)
+        px = px_ + 0.5*(vx(j) + vx(j+1))*dt;
+        py = py_ + 0.5*(vy(j) + vy(j+1))*dt;
+        pz = pz_ + 0.5*(vz(j) + vz(j+1))*dt;
+    end
 
 end
